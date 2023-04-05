37d280cd57e2ea49245ae9acf7cbab36d60ba2ab
Louis Barbier <louis.barbier@etud.univ-jfc.fr>
2023-04-05 12:37:35 +0200

    petit problème réglé

--- src/main/java/easyLabus/projet/dao/EnseignementRepository.java
+++ src/main/java/easyLabus/projet/dao/EnseignementRepository.java
@@ -24,7 +24,7 @@ public interface EnseignementRepository extends JpaRepository<Enseignement, Stri
 
     // NE MARCHE PEUT ETRE PAS //
     @Query(nativeQuery = true, value =
-           "SELECT pers.prenompers as prenompers, pers.nompers as nompers, pers.numtel as nNumtel, pers.identifiant as identifiant, pers.email as email, pers.coordprivee as coordprivee "
+           "SELECT pers.prenompers as prenompers, pers.nompers as nompers, pers.numtel as numtel, pers.identifiant as identifiant, pers.email as email, pers.coordprivee as coordprivee "
           +"FROM enseigner er INNER JOIN Personneinterne pers ON er.identifiant = pers.identifiant "+
                    "INNER JOIN Enseignement ens ON er.codeens=ens.codeens "
           +"WHERE ens.codeens = :code ")
--- src/main/java/easyLabus/projet/dto/IntervenantENSSimple.java
+++ src/main/java/easyLabus/projet/dto/IntervenantENSSimple.java
@@ -4,7 +4,7 @@ public interface IntervenantENSSimple {
     String getIdentifiant();
     String getPrenompers();
     String getNompers();
-    String getnNumtel();
+    String getNumtel();
     String getEmail();
     Boolean getCoordprivee();
 }
