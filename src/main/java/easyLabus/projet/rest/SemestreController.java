d59bfcf50a35394851749f959dc21a0e42789d89
Louis Barbier <louis.barbier@etud.univ-jfc.fr>
2023-04-11 10:52:17 +0200

    essai pour voir ce qui ne marche pas

--- src/main/java/easyLabus/projet/rest/SemestreController.java
+++ src/main/java/easyLabus/projet/rest/SemestreController.java
@@ -29,10 +29,11 @@ public class SemestreController {
 
 
     @PostMapping("AddUe")
-    public void addUe(
+    public String addUe(
         @RequestBody Map<String, String> newUeJSON,
         @RequestParam(required = true) Long idsemestre) {
 
         semestreservice.addNewUe(newUeJSON.get("codeue"),newUeJSON.get("intituleue"), idsemestre);
+        return "test";
     }
 }
